9ce42eab1fbc0533ad8848c7998a9bcf030063db
Ben Cowden <benjcowden@gmail.com>
2018-10-31 14:11:18 -0700

    Add #stock_picker method and tests

--- TDD/lib/tdd.rb
+++ TDD/lib/tdd.rb
@@ -7,6 +7,42 @@ class Array
   end
   
   def two_sum
-    
+    array = []
+    (0...self.length).each do |i|
+      (i+1...self.length).each do |j|
+        array << [i,j] if self[i] + self[j] == 0
+      end 
+    end 
+    array
+  
   end
+end
+
+def my_transpose(array)
+  len = array.length
+  result = Array.new(len) { Array.new(len) }
+  (0...len).each do |i|
+    (0...len).each do |j|
+      result[i][j] = array[j][i]
+    end 
+  end 
+  result
+end
+
+
+def stock_picker(array)
+  result = []
+  current_profit = 0
+  len = array.length
+  
+  (0...len).each do |i|
+    (0...len).each do |j|
+      potential = array[j] - array[i]
+      if potential > current_profit
+        current_profit = potential 
+        result = [i, j]
+      end
+    end 
+  end 
+  result
 end
--- TDD/spec/tdd_spec.rb
+++ TDD/spec/tdd_spec.rb
@@ -18,4 +18,40 @@ RSpec.describe do
       expect([-1, 0, 2, -2, 1].two_sum[1]).to eq([2, 3])
     end 
   end
+  
+  describe "#my_transpose" do 
+    array = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]
+  
+    it "should return array of same length as input array" do
+      expect(my_transpose(array).length).to eq(3)
+    end 
+    
+    it "correctly transposes the array" do
+      expect(my_transpose(array)).to eq(array.transpose)
+    end
+  end 
+  
+  describe "#stock_picker" do
+    it "should return an array" do 
+      array = [0,1]
+      expect(stock_picker(array).class).to be(Array)
+    end
+    
+    it "should return a pair of indices" do 
+      array = [0,1,2,3]
+      expect(stock_picker(array).length).to eq(2)
+    end
+    
+    it "should return the correct pair of indices" do 
+      expect(stock_picker([1,9,2,3])).to eq([0,1])
+      expect(stock_picker([1,5,2,3,11])).to eq([0,4])
+      expect(stock_picker([7,5,2,3,11])).to eq([2,4])
+    end
+  end
+
+  
 end 
