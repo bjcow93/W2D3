67c65899d21d892ab71d6169b219054616261892
Ben Cowden <benjcowden@gmail.com>
2018-10-31 17:19:32 -0700

    Fix high card

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -4,7 +4,25 @@ require_relative "deck"
 class Hand
   attr_accessor :cards
   
-  def initialize
+  def initialize(deck)
     @cards = []
+    @deck = deck.cards
+  end
+  
+  def take_card(num)
+    num.times {@cards << @deck.pop}
+  end
+  
+  def high_card
+    @cards.sort_by { |card| Card::VALUES[card.value]}.last
   end
 end 
+
+if __FILE__ == $PROGRAM_NAME
+  deck = Deck.new
+  deck.shuffle_deck
+  hand = Hand.new(deck)
+  hand.take_card(5)
+  p hand.cards
+  p hand.high_card
+end
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe do
         expect(hand.cards).to eq([])
       end
     end
+    describe "#take_card" do 
+      it "takes the specified number of cards from the deck"
+    end
+    describe "#high_card" do 
+      it "returns the highest card in the hand"
+    end 
   end
 end
